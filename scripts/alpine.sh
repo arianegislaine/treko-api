c5effee9f150b9cfecc8533fe054c8427fd01b80
Ariane Gislaine <36931659+arianegislaine@users.noreply.github.com>
2020-11-07 21:37:34 -0300

    Create alpine.sh
    informando versão do mongo.

---
+++ scripts/alpine.sh
@@ -0,0 +1,4 @@
+echo 'http://dl-cdn.alpinelinux.org/alpine/v3.6/main' >> /etc/apk/repositories
+echo 'http://dl-cdn.alpinelinux.org/alpine/v3.6/community' >> /etc/apk/repositories
+apk update
+apk add --no-cache mongodb=4.1.13-r0
